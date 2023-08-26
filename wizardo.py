7bb216ef939aafb1cbdcbc7e5e69cd529d1b1fd1
Lachlanna <lachlainn.hayes@gmail.com>
2023-08-26 23:36:05 +0100

    i dk if it pushed last time

--- wizardo.py
+++ wizardo.py
@@ -1,15 +1,14 @@
 import random
 
 output = ""
-newEndings = ["lamo!!","zaaam!!", "!!", "shwiz!", "kadabra!", "kazaam!"]
+newEndings = ["lamo!!","zaaam!!", "!!", "shwiz!", "kadabra!", "kazaam!"] #to be appended later. might make into a long text file later
 
 
-def Randomise(lines):
-    ran = random.randint(1,lines)
-    return ran
+def Randomise(lines): #apparently this helps with not rolling the name number over and over??
+    r = random.randint(1,lines)
+    return r
 
-
-def LineCount(file):
+def LineCount(file): #counts the number of lines in the file to pass to the RNG in the Phoneme randomiser
     with open(file,'r') as f:
        li = f.readlines()
     total_line = len(li)
@@ -18,7 +17,7 @@ def LineCount(file):
 
 def Phoneme(file):
     lines = LineCount(file)
-    with open(file) as f:        
+    with open(file) as f:         # reads the txt file for either a vowel or a consonant
         r = Randomise(lines)
         content = f.readlines()
 #         print("i counted ",lines,"lines in that document")
@@ -26,7 +25,7 @@ def Phoneme(file):
         return content[r].strip()
     
     
-for i in range(random.randint(3, 7)):
+for i in range(random.randint(3, 7)): #puts the acutal word together!
     r = random.randint(0, 1)
     if r == 0:
         f = "conE.txt"
@@ -36,4 +35,4 @@ for i in range(random.randint(3, 7)):
     
 
     
-print((output + random.choice(newEndings)).capitalize())
+print((output + random.choice(newEndings)).capitalize()) #maybe i should make the txt files into big strings and just use random.choice?
